70a469d19605aed3a69a364fdf2458d49fcd5ef8
Elgar Teo <elgarteo95@gmail.com>
2019-07-11 16:58:51 +0800

    Improve searching algorithm

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -17,26 +17,3 @@ check_limit <- function (n, verbose) {
   }
   n
 }
-
-## Function to search index of search term among multiple columns
-search_columns <- function(search_term, split_chr, ...) {
-  name_list <- list(...)
-  index <- {}
-  search_term <- unlist(strsplit(search_term, split_chr))
-  for (i in 1:length(name_list[[1]])) {
-    for (n in 1:length(name_list)) {
-      tmp <- unlist(strsplit(name_list[[n]][i], split_chr)) # Split into individual characters/words
-      for (j in 1:length(search_term)) {
-        match_boolean <- ifelse(TRUE %in% (tmp %in% search_term[j]), TRUE, FALSE)
-        if (match_boolean) {
-          break
-        }
-      }
-      if (match_boolean) {
-        index <- c(index, i)
-        break
-      }
-    }
-  }
-  index
-}
